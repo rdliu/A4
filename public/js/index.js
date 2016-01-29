67c06c6171e8c11ea61c4db13f0fdd13d6a0804d
Spencer Woo <sw92821@aol.com>
2016-01-29 06:11:53 +0000

    Project centered

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var website = "http://a4-proto.herokuapp.com/";
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
@@ -10,12 +11,16 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#submitBtn").click(submitInfo);
-}
 
+}
 
 function submitInfo(e) {
 	//for now just load next page
 
-	$("#submitBtn").text("TEST");
-	window.open("/views/splash", "_blank");
+	//$("#submitBtn").text("TEST");
+	//window.open("splash", "_blank");
+	//window.location = website + 'project/splash.htm';
+	var userName = document.getElementById('inputUsername');
+	//document.getElementById('loginForm').inputUsername;
+	window.open("/project/" + userName.value, "_self");
 }
