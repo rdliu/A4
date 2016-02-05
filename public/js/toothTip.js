83944d2712190ddeb8728dfea32ed0b576672439
Spencer Woo <smwoo2016@gmail.com>
2016-02-04 20:38:00 -0800

    iphone6

--- public/js/toothTip.js
+++ public/js/toothTip.js
@@ -4,13 +4,15 @@ $(document).ready(function() {
 
 function initializeToothTip() {
 	var tips = [
-	"tip1",
-	"tip2",
-	"tip3",
-	"tip4"
+	"A soft bristled electric toothbrush is the way to go!", 
+	"Remember to brush your gums, tongue, and roof of your mouth!", 
+	"Don't rinse after brushing your teeth to get the most out of your toothpaste!",
+	"Dentists recommend two minutes of thorough brushing!",
+	"Flossing is not to be underestimated!",
+	"Circular brushing, like polishing a shoe, is the most effective method to remove plaque!"
 	];
 
-	var max = 3;
+	var max = 5;
 	var min = 0;
 	var n = Math.floor(Math.random() * (max - min + 1)) + min;
 
